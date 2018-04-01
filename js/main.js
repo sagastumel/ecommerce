63fb71aac61aff066467692ffe6923c0e642b3b4
sagastumel <sagastumel@yahoo.com>
2018-03-31 20:09:11 -0700

    Small update of javascript now with bikes information

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 // My Header
 
-var div1 = document.createElement('div');
+var h1Element = document.createElement('h1');
 
-var body = document.getElementById('java').appendChild(div1);
-var header = document.createElement('h1');
 var h1Text = document.createTextNode('My Little Market');
 
-div1.appendChild(header);
+h1Element.appendChild(h1Text);
 
-header.appendChild(h1Text);
+document.getElementById('myHeader').appendChild(h1Element);
 
 //End Header
 // My first list of items (Bikes)
@@ -34,26 +32,74 @@ bikeArray.push(blackbike, orangebike, bluebike);
 
 //forloop for bike Array
 for (var bikes = 0; bikes < bikeArray.length; bikes++) {
+  var newItem = document.createElement('div');
+  var newDiv = document.createElement('div');
+  var nameH1 = document.createElement('h1');
+  var typeH3 = document.createElement('h3');
+  var speedH4 = document.createElement('h4');
+  var stockH4 = document.createElement('h4');
+  var btn = document.createElement('button');
+  var image = document.createElement('img');
+
+  //Elements for Text
+
+  var bikeName = document.createTextNode(bikeArray[bikes].name);
+  var bikeType = document.createTextNode('Availible types:' + bikeArray[bikes].type);
+  var bikeSpeed = document.createTextNode('Speed type:' + bikeArray[bikes].speed);
+  var bikeStock = document.createTextNode('Stock amount:' + bikeArray[bikes].stock);
+  var bikeButton = document.createTextNode('Ride Now!');
+
+  // image source attribute
+  image.src = bikeArray[bikes].image;
+
+  //class attributes
+  image.className = 'img-responsive';
+  btn.className = 'btn btn-primary btn-lg';
+  newItem.className = 'col=sm-4';
+  newDiv.className = 'bikeName' + [bikes] + 'thumbnail';
+
+  //adding text to my Elements
+  nameH1.appendChild(bikeName);
+  typeH3.appendChild(bikeType);
+  speedH4.appendChild(bikeSpeed);
+  stockH4.appendChild(bikeStock);
+  btn.appendChild(bikeButton);
+
+  //adding elements to new divs
+  newDiv.appendChild(nameH1);
+  newDiv.appendChild(image);
+  newDiv.appendChild(typeH3);
+  newDiv.appendChild(speedH4);
+  newDiv.appendChild(stockH4);
+  newDiv.appendChild(btn);
+
+  //adding divs to new item dive
+  newItem.appendChild(newDiv);
+
+  //adding new item to the element with
+  document.getElementById('bikes').appendChild(newItem);
 
 }
-//Bike Construc
 
-var div1 = document.createElement('div');
-document.getElementById('java').appendChild(div1);
+//
+// //Bike Construc
+//
+// var div1 = document.createElement('div');
+// document.getElementById('java').appendChild(div1);
+//
+// // BEANIES
+// var div2 = document.createElement('div');
+// document.getElementById('java').appendChild(div2);
+//
+// // SHIRTS
+// var div3 = document.createElement('div');
+// document.getElementById('java').appendChild(div3);
+//
+// // PANTS
+// var div4 = document.createElement('div');
+//
+// document.getElementById('java').appendChild(div4);
 
-// BEANIES
-var div2 = document.createElement('div');
-document.getElementById('java').appendChild(div2);
-
-// SHIRTS
-var div3 = document.createElement('div');
-document.getElementById('java').appendChild(div3);
-
-// PANTS
-var div4 = document.createElement('div');
-
-document.getElementById('java').appendChild(div4);
-
-// SHOES
-var div5 = document.createElement('div');
-document.getElementById('java').appendChild(div5);
+// // SHOES
+// var div5 = document.createElement('div');
+// document.getElementById('java').appendChild(div5)
